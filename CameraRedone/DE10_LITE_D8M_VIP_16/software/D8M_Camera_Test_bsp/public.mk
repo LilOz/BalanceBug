21800df60158d2bba3fc54a2a1f14465ff45fa10
MichaelChung888 <michaelchung88888@gmail.com>
2023-06-22 11:49:01 +0100

    .hex files for pof file

--- CameraRedone/DE10_LITE_D8M_VIP_16/software/D8M_Camera_Test_bsp/public.mk
+++ CameraRedone/DE10_LITE_D8M_VIP_16/software/D8M_Camera_Test_bsp/public.mk
@@ -157,9 +157,9 @@ SOPC_SYSID_FLAG += --sidp=0x410e0
 ELF_PATCH_FLAG  += --sidp 0x410e0
 
 # The SOPC Timestamp 
-# setting SOPC_TIMESTAMP is 1687186937
-SOPC_SYSID_FLAG += --timestamp=1687186937
-ELF_PATCH_FLAG  += --timestamp 1687186937
+# setting SOPC_TIMESTAMP is 1687429444
+SOPC_SYSID_FLAG += --timestamp=1687429444
+ELF_PATCH_FLAG  += --timestamp 1687429444
 
 # Enable JTAG UART driver to recover when host is inactive causing buffer to 
 # full without returning error. Printf will not fail with this recovery. none 
